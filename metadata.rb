169eff2db43431fa73e1c6e7a70e053795f7347b
Kostas Georgakopoulos <kostas.georgakopoulos@rackspace.co.uk>
2015-07-20 08:59:56 +0100

    Fixed description

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ name 'user_shadow'
 maintainer 'Rackspace'
 maintainer_email 'rackspace-cookbooks@rackspace.com'
 license 'Apache 2.0'
-description 'Provides a LWRP to modify shadow attributes for a user'
+description 'Provides a resource to modify shadow attributes for a user'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
 
